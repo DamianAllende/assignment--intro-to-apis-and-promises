bab0b58fffca10d2ff976a11f2e7eb74115b5e90
DamianAllende <amigosaburridos@gmail.com>
2018-03-06 18:37:50 -0600

    HomeWork Damian Allende

--- starter-files/js/forEach.js
+++ starter-files/js/forEach.js
@@ -5,21 +5,23 @@ request
 	.then(function(data){
 
 		let container = document.querySelector(".countries-container")
-		let paises = data.body
+		let nomPais = data.body
 		let pais = ""
 		let capital = ""
 		let flag = ""
 		
-		for(let i = 0; i < paises.length; i++){
-			pais = paises[i].name
-			capital = paises[i].capital
-			flag = paises[i].flag
+		nomPais.forEach(function(element){
+			//console.log(element.name)
+			let pais = element.name
+			let capital = element.capital
+			let flag = element.flag
 			let country = document.createElement("div")
 			country.classList.add("country-card")
 			container.appendChild(country)
 			let image = document.createElement("img")
 			image.classList.add("country-flag")
 			image.src = flag
+			country.appendChild(image)
 			country.appendChild(image)
 			let nombre = document.createElement("h4")
 			nombre.classList.add("country-name")
@@ -29,11 +31,6 @@ request
 			capi.textContent = capital
 			capi.classList.add("country-capital")
 			country.appendChild(capi)
-
-
-
-		}
-		
-
+		})
 
 	})
